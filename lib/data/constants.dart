2906304f69499c2c147c3c73dae0d85b9055adf4
Uzziel Palma <uzzielpalma99@gmail.com>
2025-12-11 17:55:46 -0600

    Cambio las constantes a Acceptance

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,65 +1,8 @@
 library;
 // // TEST Services List
 
-// /// <<<<<<<< Embebed Forms Web >>>>>>>>>
-// ///
-// const String urlBaseEmbed = 'https://www.acceptance.com/';
-
-// const String urlBaseEmbedTriton = 'https://triton.acceptance.com/';
-
-// const String urlBaseEmbedTritonInsurance =
-//     'https://triton.acceptanceinsurance.com/';
-
-// const String urlBaseEmbedQuickPay = 'https://quickpay.acceptance.com/';
-
-// /// <<<<<<<< User Profile & Policy >>>>>>>>>
-// /// <<<<<<<< Login & Registration >>>>>>>>>
-
-// const String envLogin = String.fromEnvironment(
-//   'env',
-//   defaultValue: 'https://confie-customer-np.azurewebsites.net',
-// );
-
-// const String apiKeyLogin =
-//     'jEk40pLbflj4vQ6RyhQmI3JxDAXjUhdWrEjYBgQRAuSs8X6ged161peEtM4mM8sT';
-
-// /// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
-
-// const String envWallet = String.fromEnvironment(
-//   'env',
-//   defaultValue: 'https://confie-wallet-api-np.azurewebsites.net',
-// );
-
-// const String apiKeyWallet = 'GfhGdjdx3rfGBBFkf';
-
-// /// <<<<<<<< Office Locations >>>>>>>>>
-
-// const String envOffice = String.fromEnvironment(
-//   'env',
-//   defaultValue: 'https://stg-inquiry.confie.com',
-// );
-
-// const String apiKeyOffice = 'fjzzkOuCefd8-Z86i9HMGWQ=';
-
-// const String legalEntity = 'Acceptance Insurance Agency Of Tennessee, Inc.';
-
-// /// <<<<<<<< Offices Map >>>>>>>>>
-// /// <<<<<<<< Thirds Party >>>>>>>>>
-
-// const String envThirdsPartyZipcode = 'https://api.zippopotam.us/us/';
-
-// const String envThirdsPartyAppleMap = 'https://maps.apple.com/';
-
-// const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
-
-// /// <<<<<<<< Brand >>>>>>>>>
-
-// const String brandConstant = 'Acceptance';
-
-// TEST Services List
-
 /// <<<<<<<< Embebed Forms Web >>>>>>>>>
-
+///
 const String urlBaseEmbed = 'https://www.acceptance.com/';
 
 const String urlBaseEmbedTriton = 'https://triton.acceptance.com/';
@@ -111,4 +54,61 @@ const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
 
 /// <<<<<<<< Brand >>>>>>>>>
 
-const String brandConstant = 'Freeway';
+const String brandConstant = 'Acceptance';
+
+// // TEST Services List
+
+// /// <<<<<<<< Embebed Forms Web >>>>>>>>>
+
+// const String urlBaseEmbed = 'https://www.acceptance.com/';
+
+// const String urlBaseEmbedTriton = 'https://triton.acceptance.com/';
+
+// const String urlBaseEmbedTritonInsurance =
+//     'https://triton.acceptanceinsurance.com/';
+
+// const String urlBaseEmbedQuickPay = 'https://quickpay.acceptance.com/';
+
+// /// <<<<<<<< User Profile & Policy >>>>>>>>>
+// /// <<<<<<<< Login & Registration >>>>>>>>>
+
+// const String envLogin = String.fromEnvironment(
+//   'env',
+//   defaultValue: 'https://confie-customer-np.azurewebsites.net',
+// );
+
+// const String apiKeyLogin =
+//     'jEk40pLbflj4vQ6RyhQmI3JxDAXjUhdWrEjYBgQRAuSs8X6ged161peEtM4mM8sT';
+
+// /// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
+
+// const String envWallet = String.fromEnvironment(
+//   'env',
+//   defaultValue: 'https://confie-wallet-api-np.azurewebsites.net',
+// );
+
+// const String apiKeyWallet = 'GfhGdjdx3rfGBBFkf';
+
+// /// <<<<<<<< Office Locations >>>>>>>>>
+
+// const String envOffice = String.fromEnvironment(
+//   'env',
+//   defaultValue: 'https://stg-inquiry.confie.com',
+// );
+
+// const String apiKeyOffice = 'fjzzkOuCefd8-Z86i9HMGWQ=';
+
+// const String legalEntity = 'Acceptance Insurance Agency Of Tennessee, Inc.';
+
+// /// <<<<<<<< Offices Map >>>>>>>>>
+// /// <<<<<<<< Thirds Party >>>>>>>>>
+
+// const String envThirdsPartyZipcode = 'https://api.zippopotam.us/us/';
+
+// const String envThirdsPartyAppleMap = 'https://maps.apple.com/';
+
+// const String envThirdsPartyGoogleMap = 'https://www.google.com/maps/';
+
+// /// <<<<<<<< Brand >>>>>>>>>
+
+// const String brandConstant = 'Freeway';
