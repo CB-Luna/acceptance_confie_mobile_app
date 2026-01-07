a00048e6b768732f11a9efcf00005ca9333164a7
Uzziel Palma <uzzielpalma99@gmail.com>
2026-01-07 17:33:12 -0600

    Nueva Versión para mapas en iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDXp_4UCVX5nDIe_emxzr2IPRB1jixpgjA")
+    GMSServices.provideAPIKey("AIzaSyDB7DprfB88jSMx-1R7cbZYo-Xm_MazXpU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
