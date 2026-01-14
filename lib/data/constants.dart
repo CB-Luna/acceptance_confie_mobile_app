0dde02a8f694d2e40514495801aacd46f80ba3b5
Uzziel Palma <uzzielpalma99@gmail.com>
2026-01-14 16:30:22 -0600

    Nuevo icono de la App

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,5 +1,5 @@
 library;
-// // TEST Services List
+// // PROD Services List
 
 /// <<<<<<<< Embebed Forms Web >>>>>>>>>
 ///
@@ -17,20 +17,20 @@ const String urlBaseEmbedQuickPay = 'https://quickpay.acceptance.com/';
 
 const String envLogin = String.fromEnvironment(
   'env',
-  defaultValue: 'https://confie-customer-np.azurewebsites.net',
+  defaultValue: 'https://confie-customer.azurewebsites.net',
 );
 
 const String apiKeyLogin =
-    'jEk40pLbflj4vQ6RyhQmI3JxDAXjUhdWrEjYBgQRAuSs8X6ged161peEtM4mM8sT';
+    'pjfPNCB2zLFgfDrSu6OdQSyvoscBbIn19Jj3jySl4eJikOy8ORqIRlD7xLb8tiH1';
 
 /// <<<<<<<< PK Pass Wallet (Google / Apple) >>>>>>>>>
 
 const String envWallet = String.fromEnvironment(
   'env',
-  defaultValue: 'https://confie-wallet-api-np.azurewebsites.net',
+  defaultValue: 'https://confie-wallet-api.azurewebsites.net',
 );
 
-const String apiKeyWallet = 'GfhGdjdx3rfGBBFkf';
+const String apiKeyWallet = 'Hwsed7698FdhskG5lkkg';
 
 /// <<<<<<<< Office Locations >>>>>>>>>
 
@@ -111,4 +111,4 @@ const String brandConstant = 'Acceptance';
 
 // /// <<<<<<<< Brand >>>>>>>>>
 
-// const String brandConstant = 'Freeway';
+// const String brandConstant = 'Acceptance';
